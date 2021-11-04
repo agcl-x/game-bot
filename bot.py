2c352a20447f7fbbd7e1726750984b95d4567f3b
Fuckuraso <85117295+Fuckuraso@users.noreply.github.com>
2021-11-04 14:28:51 +0200

    Add files via upload

--- bot.py
+++ bot.py
@@ -11,65 +11,8 @@ voices=[]
 client = commands.Bot(command_prefix= str(pr) )
 client.remove_command("help")
 @client.command()
-async def party_ac(ctx):
-    global id, n, k, sd, channel,voices,kik
-    auth=str(ctx.author.id)
-    for i in voices:
-        channel = client.get_channel(int(i)) #gets the channel you want to get the list from
-        members = channel.members 
-        memids = [] #(list)
-        for member in members:
-            memids.append(member.id)
-    if auth in n and ctx.author.voice !=None:
-        await ctx.reply('*_Тепер ти в паті!!_*')
-        for i in sd:
-            channel = client.get_channel(int(i)) 
-            members = channel.members
-            for j in k:
-                if j in members:
-                    kik+=1
-                    await ctx.author.move_to(channel)
-                    await channel.set_permissions(ctx.guild.default_role, connect=False)
-    elif ctx.author.voice == None:
-        await ctx.send('Зайди в любий войс чат')
-    elif auth not in n:
-        await ctx.send('Тебе ніхто не запрошував!')   
-@client.command()
-async def party(ctx, a1:discord.Member):
-    global id, sd, k, ch, channel, n, voices
-    #messages
-    #lists
-    id = str(a1.id)
-    ch+=1
-    n.append(id)
-    k.append(ctx.author)
-    for guild in client.guilds:
-        mainc = discord.utils.get(guild.categories, id=855389853134225408)
-    if ctx.author.voice == None:  
-        await ctx.send('Зайди в любий войс чат')
-    elif ctx.author.voice != None:
-        await a1.send('Привітики батончики. Тебе запросили в войс!')
-        await ctx.reply('Запрошення надіслане. Очікуйте!')
-        chan = await guild.create_voice_channel(f'party{ch}', category=mainc)
-        await ctx.author.move_to(chan)
-        sd.append(str(chan.id))
-        voices.append(str(chan.id))
-@client.event
-async def on_voice_state_update(member:discord.Member, before, after):
-    if before.channel is None and after.channel is not None:
-        print('.')
-    if after.channel is None and before.channel is not None:
-        channelid2 = before.channel.id
-        channel = client.get_channel(channelid2) #gets the channel you want to get the list from
-        members = channel.members #finds members connected to the channel
-        memids = [] #(list)
-        for member in members:
-            memids.append(member.id)
-        if memids==[]:
-            for i in voices:
-                existing_channel = discord.utils.get(guild.channels, id=int(i))
-                await existing_channel.delete() 
-
+async def help(ctx):
+    await ctx.send("Доступні: racer, Sl(souls like), film(інтерактивне кіно), re(remastered,redux,remake), stels, sim(симулятори), pmr(платформер),cop(c-op),puzzle(головоломка), adven(пригоди)")
 @client.command()
 async def game(ctx,*, a1:str):
     file = Path('db.xlsx')
@@ -78,34 +21,40 @@ async def game(ctx,*, a1:str):
     n=0
     a=[]
     k=0
+    mn=0
     for row in ws.values:
         n+=1
-        lst=list(row)
-        if lst[0]!=None and lst[1]!=None:
-            print(lst[1])
-            lstt=list(lst[1])
-            lst2=list(lst[3])
-            #print(lst2)
-            
-            if ',' in lst2:
-                lst4=lst[3].replace(' ', '').split(',')
-                
-            else:
-                lst4=lst[3]
-                
-            if ',' in lstt:
-                lst1=lst[1].replace(' ', '').split(',')
-            else:
-                lst1=lst[1]
-            if a1 in lst1 or a1 in row or a1 in lst4:
-                a.append(n)
-        elif lst[0]==None or lst[1]==None:
-            print('.')
+        main=list(row)
+        if n>1:
+            r_l=[str(i).lower() for i in main]
+            #print(r_l)
+            if r_l[0]!=None and r_l[1]!=None:
+                dev=r_l[1]
+                ganre=r_l[3]
+                if ',' in ganre:
+                    all_gan=r_l[3].replace(' ', '').split(',')                
+                else:
+                    all_gan=r_l[3]
+                    
+                if ',' in dev:
+                    all_dev=r_l[1].replace(' ', '').split(',')
+                else:
+                    all_dev=r_l[1]
+        #elif lst[0]==None or lst[1]==None:
+            #print('.')
+                name=r_l[0].replace(':', ' ').split(' ')
+            #print(name)
+                game = r_l[0]
+                if (a1 in r_l) or (a1 in all_gan) or (a1 in all_dev) or (a1 in name):
+                    a.append(n)
+                    print(a)
+        else:
+            pass
     l=int(len(a))
     if l==1:
         for row in ws.values:
             k+=1
-            if k == ap:
+            if k == n:
                 year=str(row[2])
                 y=year[0]+year[1]+year[2]+year[3]
                 em = discord.Embed(title='Your game is %s'%row[0], description = f'Download link:{row[13]}', color=discord.Colour.purple())
