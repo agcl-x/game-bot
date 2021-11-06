f1f5080a0def00b22bdc2de709a6a54175a3aa4f
Fuckuraso <85117295+Fuckuraso@users.noreply.github.com>
2021-11-06 13:25:58 +0200

    Add files via upload

--- bot.py
+++ bot.py
@@ -10,16 +10,52 @@ kik=0
 voices=[]
 client = commands.Bot(command_prefix= str(pr) )
 client.remove_command("help")
-@client.command()
+@client.command(invoke_without_command=True)
 async def help(ctx):
-    await ctx.send("Доступні: racer, Sl(souls like), film(інтерактивне кіно), re(remastered,redux,remake), stels, sim(симулятори), pmr(платформер),cop(c-op),puzzle(головоломка), adven(пригоди)")
+    em = discord.Embed(title='Help',description="{}-не потрібно писати", color=discord.Colour.purple())
+    em.add_field(name = 'Ігри', value = '.game {щось небудь (назва, рік, розробник, або ключове слово(щось по типу жанру))}\nНа разі доступні ключові слова: racer, Sl(souls like), film(інтерактивне кіно), re(remastered,redux,remake), stels, sim(симулятори), pmr(платформер),cop(c-op),puzzle(головоломка), adven(пригоди)',inline=False)
+    em.add_field(name = 'Спам', value = '.spam {к-ть повідомлень} , { фраза}',inline=False)
+    em.add_field(name = 'Змінити статус', value = '.change_status - (Тільки для адміна)',inline=False)
+    await ctx.send(embed = em)
 @client.command()
-async def game(ctx,*, a1:str):
+async def spam(ctx, a1, *, a2):
+    for i in range(0,int(a1)):
+        await ctx.send(str(a2))
+        time.sleep(0.7)
+@client.event
+async def on_voice_state_update(member:discord.Member, before, after):
+    global voices
+    guild= discord.utils.get(client.guilds, id=794363687497760770)
+    for guild in client.guilds:
+        mainc = discord.utils.get(guild.categories, id=794363687497760770)
+    if before.channel is None and after.channel is not None:
+        channelid1 = after.channel.id
+        if channelid1 == 906466935271727144:
+            chan = await guild.create_voice_channel(f'{member.name}party', category=mainc)
+            await member.move_to(chan)
+            voices.append(chan.id)
+    if after.channel is None and before.channel is not None:
+        for i in voices:
+            channel = client.get_channel(i) #gets the channel you want to get the list from
+            if channel is not None:   
+                members = channel.members #finds members connected to the channel
+                memids = [] #(list)
+                for member in members:
+                    memids.append(member.id)
+                if memids==[]:
+                    existing_channel = discord.utils.get(guild.channels, id=i)
+                    await existing_channel.delete()
+                    voices.remove(i) 
+            else:
+                print("loh") 
+@client.command()
+async def game(ctx,*, arg:str):
     file = Path('db.xlsx')
     wb = openpyxl.load_workbook(file) 
     ws = wb.active
     n=0
     a=[]
+    a1=arg.lower()
     k=0
     mn=0
     for row in ws.values:
@@ -77,8 +113,10 @@ async def game(ctx,*, a1:str):
                 em.add_field(name = 'Min', value = f'OS:{row[7]}\nCPU:{row[5]}\nGPU:{row[6]}\nRAM:{row[4]}\nFree place:{row[8]}', inline=False)
                 em.add_field(name = 'Max', value = f'OS:{row[12]}\nCPU:{row[10]}\nGPU:{row[11]}\nRAM:{row[9]}\nFree place:{row[8]}', inline=False)
                 await ctx.send(embed = em)
-
-            
+@client.command
+@commands.has_any_role("Admin GAY")
+async def change_status(ctx,a1:str):
+    await client.change_presence(status=discord.Status.online, activity=discord.Game(name=a1))           
 
 @client.event
 async def on_ready():
