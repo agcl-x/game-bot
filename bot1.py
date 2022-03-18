0307c7640ab03cc3bc6139d9832ea2a2f40a927e
Fuckuraso <85117295+Fuckuraso@users.noreply.github.com>
2022-03-18 22:57:50 +0200

    Add files via upload

--- bot1.py
+++ bot1.py
@@ -1,9 +1,9 @@
-import openpyxl
-from pathlib import Path
+#import openpyxl
+#from pathlib import Path
 import pymysql
-file = Path('db.xlsx')
-wb = openpyxl.load_workbook(file) 
-ws = wb.active
+#file = Path('db.xlsx')
+#wb = openpyxl.load_workbook(file) 
+#ws = wb.active
 def start(k):
 	for i in range(len(k)):
 		print(k[i])
@@ -20,23 +20,35 @@ connection = pymysql.connect(host='167.71.38.226',
                              cursorclass=pymysql.cursors.DictCursor)
 
 cursor = connection.cursor()
+def search():
+	zap="Forza"
+	pidh=[]
+	sql = f"""SELECT * FROM `id_name` WHERE 1"""
+	cursor.execute(sql)
+	result = cursor.fetchall()
+	print(result)
+	for i in result:
+		nume=i['name'].lower()
+		if zap.lower() in nume:
+			pidh.append(i['id'])
+	print(pidh)
 def ch(j):
 	try:
 		int(j)
 	except Exception:
 		print(".")
-a=[]
-for row in ws.values:
-    main=list(row)
-    a.append(main[2])
+#a=[]
+#for row in ws.values:
+    #main=list(row)
+    #a.append(main[2])
 #print(a)
-for x in a:
-	print(x)
-	if x is None or x=="Year":
-		a.remove(x)
-		print(a)
+#for x in a:
+	#print(x)
+	#if x is None or x=="Year":
+		#a.remove(x)
+		#print(a)
 #start(a)
-
+search()
 connection.commit()
 
 cursor.close()
