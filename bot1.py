772119cb22bb3a8abc3e03c773bb1181e69a0b15
Fuckuraso <85117295+Fuckuraso@users.noreply.github.com>
2022-03-18 20:15:57 +0200

    Add files via upload

---
+++ bot1.py
@@ -0,0 +1,46 @@
+import openpyxl
+from pathlib import Path
+import pymysql
+file = Path('db.xlsx')
+wb = openpyxl.load_workbook(file) 
+ws = wb.active
+def start(k):
+	for i in range(len(k)):
+		print(k[i])
+		sql = f"""INSERT INTO `id_year`(`id`, `year`) VALUES ('{i}','{k[i]}');"""
+		cursor.execute(sql)
+		result = cursor.fetchall()
+		print(result)
+connection = pymysql.connect(host='167.71.38.226',
+                             user='test',
+                             password='test',
+                             db='gamebot',
+                             port=3306,
+                             charset='utf8mb4',
+                             cursorclass=pymysql.cursors.DictCursor)
+
+cursor = connection.cursor()
+def ch(j):
+	try:
+		int(j)
+	except Exception:
+		print(".")
+a=[]
+for row in ws.values:
+    main=list(row)
+    a.append(main[2])
+#print(a)
+for x in a:
+	print(x)
+	if x is None or x=="Year":
+		a.remove(x)
+		print(a)
+#start(a)
+
+connection.commit()
+
+cursor.close()
+connection.close()
+
+
+    	
---
+++ new.py
@@ -0,0 +1,21 @@
+import time , discord, openpyxl, random
+from discord.ext import commands
+from pathlib import Path
+ch=0
+pr='.'
+n=[]
+k=[]
+sd=[]
+kik=0
+voices=[]
+client = commands.Bot(command_prefix= str(pr) )
+client.remove_command("help")
+def get()
+@client.event
+async def on_ready():
+    await client.change_presence(status=discord.Status.online, activity=discord.Game(name='Fornite'))
+
+
+client.run('ODU0Mzg5NDE0OTA2NDI5NDcw.YMjOJg.68ReXhYTPFZrTNhOAL3fyiuI4rA')
+#channel1 = ctx.author.voice.channel
+    #await channel.connect()
